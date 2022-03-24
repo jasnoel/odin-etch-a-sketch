e7d8b8dc10b149cda7a4df132ce78913d6c5c862
jasnoel <jason.noel.imp@gmail.com>
2022-03-24 15:28:31 +0100

    added reset panel button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const SQUARE_DIMENSIONS = 16;
 
+
+const squares = [];
 const container = document.querySelector(".container");
 
 for (let i = 0; i < SQUARE_DIMENSIONS; i++) { 
@@ -10,6 +12,14 @@ for (let i = 0; i < SQUARE_DIMENSIONS; i++) {
         subDiv.addEventListener('mouseover', (e) => {
             e.target.style.backgroundColor = 'red';
         });
+        squares.push(subDiv);
         div.appendChild(subDiv);
     }
 }
+
+const resetPanel = document.querySelector(".reset-panel");
+resetPanel.addEventListener('click', () => {
+    squares.forEach(div => {
+        div.style.backgroundColor = '#fff';
+    });
+})
