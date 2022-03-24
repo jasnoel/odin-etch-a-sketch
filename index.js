0b5a24b3fa5c204aebf07068aa66877ee7412b92
jasnoel <jason.noel.imp@gmail.com>
2022-03-24 15:05:13 +0100

    created grid of 16 / 16 squares

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+const SQUARE_DIMENSIONS = 16;
+
+const container = document.querySelector(".container");
+
+for (let i = 0; i < SQUARE_DIMENSIONS; i++) { 
+    const div = document.createElement("div");
+    container.appendChild(div);
+    for (let j = 0; j < SQUARE_DIMENSIONS; j++) {
+        const subDiv = document.createElement("div");
+        div.appendChild(subDiv);
+    }
+}
