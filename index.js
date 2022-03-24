19c7f8ae4c5ff046d2efe0c1cba982ccaa72c8a4
jasnoel <jason.noel.imp@gmail.com>
2022-03-24 19:54:14 +0100

    change color and fixed default number of squares

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const SQUARE_DIMENSIONS = 16;
-
+const createGrid = (squaresDimensions) => {
+    for (let i = 0; i < squaresDimensions; i++) { 
+        const div = document.createElement("div");
+        container.appendChild(div);
+        for (let j = 0; j < squaresDimensions; j++) {
+            const subDiv = document.createElement("div");
+            subDiv.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 256) + ',0,' + Math.floor(Math.random() * 256) + ')';
+            });
+            squares.push(subDiv);
+            div.appendChild(subDiv);
+        }
+    }
+}
 
 const squares = [];
 const container = document.querySelector(".container");
-
-for (let i = 0; i < SQUARE_DIMENSIONS; i++) { 
-    const div = document.createElement("div");
-    container.appendChild(div);
-    for (let j = 0; j < SQUARE_DIMENSIONS; j++) {
-        const subDiv = document.createElement("div");
-        subDiv.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'red';
-        });
-        squares.push(subDiv);
-        div.appendChild(subDiv);
-    }
-}
 
 const resetPanel = document.querySelector(".reset-panel");
 resetPanel.addEventListener('click', () => {
     squares.forEach(div => {
         div.style.backgroundColor = '#fff';
     });
+    let numberOfSquare = null;
+    while (numberOfSquare > 100 || numberOfSquare < 0 || !numberOfSquare || typeof numberOfSquare != 'number') {
+        numberOfSquare = parseInt(prompt("How many squares per side do you want ?", 50));
+    }
+    container.innerHTML = "";
+    createGrid(numberOfSquare);
 })
+
+createGrid(16);
